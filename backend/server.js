41124e511c6c6ee08d216b7e35c947a6b04b82d6
tristanjones-student <tristanjones@myboisestate.edu>
2023-10-13 14:27:47 -0600

    added default route response

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,25 @@
-
+const choreRoutes = require("./choreRoutes");
 require('dotenv').config();
 const express = require('express');
 const cors = require("cors");
 const app = express();
 
 app.use(cors());
-const choreRoutes = require("./choreRoutes");
-app.use(express.json())
+
+app.use(express.json());
+
 app.use( (req,res,next) =>{
     console.log(req.path, req.method);
     next();
 })
 
+app.get("/", (req,res) => {
+res.json({mssg: "Use /api to get the api data"})
+});
+
+
 app.use('/api',choreRoutes);
 
 app.listen(process.env.PORT , () => {
 console.log('Running');
-});
+})
